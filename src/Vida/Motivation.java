e47f9c9960f3ed0a30eda3eb2d45e280479f5758
Vida Duodu <vidash50@gmail.com>
2021-03-04 18:09:21 -0500

    initial commit

---
+++ src/Vida/Motivation.java
@@ -0,0 +1,18 @@
+package Vida;
+
+public class Motivation {
+
+    /*
+     HELLO TEAM I JUST CREATED THIS PROJECT FOR US TO PRACTICE JAVA INTERVIEW QUESTIONS
+            1. WHATEVER JAVA QUESTION WE SOLVE HERE, LETS WRITE THE LOGIC BEHIND IT ON EACH
+            LINE SO THAT ANYONE WHO LOOKS AT THE CODE CAN UNDERSTAND IT.
+
+            2. THANK YOU TEAM ===> POSITIVE VIBES ONLY
+     */
+
+    public static void main(String[] args) {
+
+        System.out.println("POSITIVE MINDSET WILL TAKE US FAR");
+    }
+
+}
