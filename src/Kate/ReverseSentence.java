8438c6a20c58b1661fd055a7ab5e183a84799956
KatrinSi <katrinsi@me.com>
2021-04-07 15:52:28 -0400

    Added NumDividedWithoutDivision and ReverseSentence tasks Muhtar's versions

---
+++ src/Kate/ReverseSentence.java
@@ -0,0 +1,22 @@
+package Kate;
+
+public class ReverseSentence {
+
+    //Muhtar's solution
+
+//    public String StrReverse(String str) {
+//
+//        String reverse = "";
+//
+//        for (int i = str.length() - 1; i >= 0; i--) {
+//            reverse += str.toCharArray()[i];
+//        }
+//        return reverse;
+//
+//    }
+
+
+    //my solution:
+
+
+}
